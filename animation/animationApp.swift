7cf950ac712e10f81e7ce123dfc23108a29e9a9f
will <willsuosuo@gmail.com>
2026-06-15 11:24:13 +0800

    Initial Commit

---
+++ animation/animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationApp.swift
+//  animation
+//
+//  Created by will Suo on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
